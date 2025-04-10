091c98e3b5ed7ac2ca399f6a92d0614d0b170f8a
Toktobekov Aidar <aidar.omurbekov05@gmail.com>
2025-04-10 17:59:54 +0600

    Added data in state

--- src/features/efficiency/efficiencySlice.js
+++ src/features/efficiency/efficiencySlice.js
@@ -4,20 +4,88 @@ import {getRegionEfficiency} from "./efficiencyThunk.js";
 const initialState = {
     regionEfficiency: [
         {
-            pS: '2',
-            oneDay: "3,5",
-            week: "17.5",
-            TP: '19',
-            AVR: "2",
-            dismantling: '10',
-            region: 'Чуй',
-            square: "Юг",
-            build: '1',
-            connection: "15",
-            sale: '5',
-            point: '14',
-            efficiency: 85,
-        }
+            region: "Джалал-Абад",
+            squares: [
+                {
+                    square: "Базар-Коргон",
+                    data: {
+                        efficiency_points: 118,
+                        physical_force: 1,
+                        works: [
+                            {
+                                work_type: "Демонтаж",
+                                amount: 6
+                            },
+                            {
+                                work_type: "Подключение",
+                                amount: 14
+                            },
+                            {
+                                work_type: "Подключение ЮР",
+                                amount: 2
+                            },
+                            {
+                                work_type: "Техпод",
+                                amount: 41
+                            }
+                        ],
+                        one_day_norm: 3.5,
+                        five_day_norm: 17.5
+                    }
+                },
+                {
+                    square: "Джалал-Абад верхняя зона",
+                    data: {
+                        efficiency_points: 75,
+                        physical_force: 1,
+                        works: [
+                            {
+                                work_type: "Демонтаж",
+                                amount: 3
+                            },
+                            {
+                                work_type: "Подключение",
+                                amount: 15
+                            },
+                            {
+                                work_type: "Техпод",
+                                amount: 26
+                            }
+                        ],
+                        one_day_norm: 3.5,
+                        five_day_norm: 17.5
+                    }
+                },
+            ]
+        },
+        {
+            region: "Иссык-Куль",
+            squares: [
+                {
+                    square: "Ананьево",
+                    data: {
+                        efficiency_points: 39,
+                        physical_force: 2,
+                        works: [
+                            {
+                                work_type: "Демонтаж",
+                                amount: 10
+                            },
+                            {
+                                work_type: "Подключение",
+                                amount: 4
+                            },
+                            {
+                                work_type: "Техпод",
+                                amount: 27
+                            }
+                        ],
+                        one_day_norm: 3.5,
+                        five_day_norm: 17.5
+                    }
+                },
+            ]
+        },
     ],
     regionEfficiencyLoading: false,
 };
